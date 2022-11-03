22c704172a98b2991f5a948d195c4aaca2e9fd52
ekatkalinina <ekat.kal1nina@yandex.ru>
2022-11-03 22:08:31 +0300

    Добавила метод для нахождения количество футболистов, которые участвовали более чем в 5 играх.

--- main.py
+++ main.py
@@ -13,5 +13,23 @@ def print_players_with_5_matches(file_name: str):
         counter += 1
 
 
+def count_players_with_5_matches(file_name: str) -> int:
+    """
+    Находит количество футболистов, которые участвовали более чем в 5 играх
+    :param file_name: название файла, содержащий сведения о футболистах
+    :return: количество футболистов, которые участвовали более чем в 5 играх
+    """
+    with open(file_name, 'r', encoding='utf-8') as file:
+        data = file.read()
+    football_players = data.split('\n')
+    counter = 0
+    for player in football_players:
+        entry = player.split('\t')
+        if int(entry[3]) > 5:
+            counter += 1
+    return counter
+
+
 if __name__ == '__main__':
     print_players_with_5_matches('file.txt')
+    print(f"{count_players_with_5_matches('file.txt')} футболистов участвовали более чем в 5 играх.")
